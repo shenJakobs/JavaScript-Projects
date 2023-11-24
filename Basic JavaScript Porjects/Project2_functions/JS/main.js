dead30f4f6e8eeb95d9087729407b2151d2b0c21
shenJakobs <shen_justason@hotmail.com>
2023-11-23 20:32:12 -0700

    Function Assingments

---
+++ Basic JavaScript Porjects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function My_First_Function() {
+    var str = "This is a button text!"; //First variable
+    str += "Or is it 0_0"; //using += to add more to the variable 
+    document.getElementById("Button_Text") .innerHTML = str; // using document,getELementById
+}
+
