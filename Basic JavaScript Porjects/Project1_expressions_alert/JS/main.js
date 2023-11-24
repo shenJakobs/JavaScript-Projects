ea780649c3c4a0f25e00d08fe142aadd53dd391c
shenJakobs <shen_justason@hotmail.com>
2023-11-23 19:55:20 -0700

    first java assignment's

---
+++ Basic JavaScript Porjects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("sup, homie?"); //widnow.alert method
+
+document.write("Sup, homie since your not listening"); //document.write method
+
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "and this is the end of the string"; //creating two variables 
+
+document.write (Sent1 + Sent2); //concantenate variables
+
+document.write(100*55); //an expression 
